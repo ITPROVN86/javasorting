eda747bc039311ba891d11626875ff9be20b33ba
ITPROVN86 <quangltn3@fe.edu.vn>
2023-09-06 10:11:49 +0700

    Second Commit 1

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
         //1. Enter a possitive number
         element.setSize_array(library.getInt("Enter number of array", 1, 100));
         //2. Create array by generate random integer in range
-        element.setArray(library.createArray(element.size_array));
+        element.setArray(library.createArray(element.getSize_array()));
         new SortProgramming(element).run();
     }
 }
